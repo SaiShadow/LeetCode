84c43a19e5f74ac5e3c2533d9043d25eaf6723c6
Sai Srijan Papineni <92605941+SaiShadow@users.noreply.github.com>
2023-10-25 20:05:42 +0200

    Time: 0 ms (100.00%), Space: 39.2 MB (45.05%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int kthGrammar(int n, int k) {
+
+        boolean same = true;
+        n = 1 << (n - 1);
+
+        while (n != 1) {
+            n /= 2;
+
+            if (k > n) {
+                k -= n;
+                same = !same;
+            }
+        }
+        return (same ? 0 : 1);
+    }
+}
