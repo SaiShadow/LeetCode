1f86bb823788fb09a2129479ea85a630de569e40
Sai Srijan Papineni <92605941+SaiShadow@users.noreply.github.com>
2025-11-03 10:51:37 +0100

    Time: 9 ms (59.83%), Space: 93.1 MB (5.68%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -1,40 +1,21 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        char[] colorArray = colors.toCharArray();
-        if (colorArray.length <= 1) {
-            return 0;
-        }
-
         int cost = 0;
-        int currMaxCost = 0;
-        boolean prevSame = false;
-        int costInGroup = 0;
+        int maxTime = neededTime[0];
+        int sum = neededTime[0];
 
-        for (int i = 1; i < colorArray.length; i++) {
-            char curr = colorArray[i];
-            char prev = colorArray[i - 1];
-
-            if (curr == prev) {
-                if (prevSame) {
-                    currMaxCost = Math.max(currMaxCost, neededTime[i]);
-                    costInGroup += neededTime[i];
-                } else {
-                    prevSame = true;
-                    currMaxCost = Math.max(neededTime[i - 1], neededTime[i]);
-                    costInGroup += neededTime[i] + neededTime[i - 1];
-                }
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                sum += neededTime[i];
+                maxTime = Math.max(maxTime, neededTime[i]);
             } else {
-                if (prevSame) {
-                    cost += costInGroup - currMaxCost;
-                    costInGroup = 0;
-                    prevSame = false;
-                    currMaxCost = 0;
-                }
+                cost += sum - maxTime;
+                sum = neededTime[i];
+                maxTime = neededTime[i];
             }
         }
-        if (prevSame) {
-            cost += costInGroup - currMaxCost;
-        }
+
+        cost += sum - maxTime;
         return cost;
     }
 }
